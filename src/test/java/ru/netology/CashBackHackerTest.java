df6c4e6c57ce686ceed507f94cd51cec9cd9602d
Tatiana Smirnova <redkiivid@mail.ru>
2023-05-22 15:46:25 +0300

    add test for testng

---
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashBackHackerTest {
+
+     CashBackHacker cashBackHacker = new CashBackHacker();
+
+    @Test
+    public void testRemainLessThanBoundary() {
+        int amount = 500;
+        int expected = 500;
+        int actual = cashBackHacker.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainEqualToBoundary() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashBackHacker.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainGreaterThanBoundary() {
+        int amount = 1500;
+        int expected = 500;
+        int actual = cashBackHacker.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+}
