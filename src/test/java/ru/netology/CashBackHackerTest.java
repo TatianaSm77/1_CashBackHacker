61fdaa4ff159ff0d170f5be575245476dbe7330f
Tatiana Smirnova <redkiivid@mail.ru>
2023-05-22 17:31:01 +0300

    add test for test junit4-platform

--- src/test/java/ru/netology/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -1,10 +1,9 @@
 package ru.netology;
 
-import junit.framework.TestCase;
-import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CashBackHackerTest {
     CashBackHacker cashBackHacker = new CashBackHacker();
 
@@ -26,6 +25,30 @@ public class CashBackHackerTest {
 
     @Test
     public void testRemainGreaterThanBoundary() {
+        int amount = 1500;
+        int expected = 500;
+        int actual = cashBackHacker.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testRemainLessThanBoundaryJunit5() {
+        int amount = 500;
+        int expected = 500;
+        int actual = cashBackHacker.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainEqualToBoundaryJunit5() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashBackHacker.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainGreaterThanBoundaryJunit5() {
         int amount = 1500;
         int expected = 500;
         int actual = cashBackHacker.remain(amount);
